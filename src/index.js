8f0b424d2d428139c2053f741e64ef09d8e23764
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-12-02 12:58:37 +0200

    update store.js with persist

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from "redux-persist/integration/react"
-import App from './Components/App';
+import App from './components/App';
 import styles from './index.module.css';
-import store from "./redux/store"
+import { store } from "./redux/store"
+import { persistor } from "./redux/store"
 
 ReactDOM.render(
   <Provider store={store} >
-    <PersistGate >
+    <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <React.StrictMode>
           <App />

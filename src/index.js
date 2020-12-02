22880a6624f26939025e92c3e3cf7f166d1735d7
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-12-02 11:09:48 +0200

    обновил библиотеки

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from "redux-persist/integration/react"
 import App from './Components/App';
 import styles from './index.module.css';
+import store from "./redux/store"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store} >
+    <PersistGate >
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
   document.getElementById('root')
 );
 

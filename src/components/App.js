e5719ae578b2f8c692294a92cc2f35b5e8afd1a1
Oleksandr Yashin <aveodenis@gmail.com>
2020-12-03 13:19:50 +0200

    origin code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import styles from './App.module.css';
+import styles from "./App.module.css"
+import Login from "./Login/Login"
+import Registration from "./Registration/Registration"
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+	return (
+		<div>
+			{/* <Login /> */}
+			<Registration />
+		</div>
+	)
 }
 
-export default App;
+export default App

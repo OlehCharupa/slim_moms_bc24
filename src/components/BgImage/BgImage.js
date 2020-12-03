40f1d9b8ff0d3654e128cc578a388aca475f763a
Alina Pavliuk <alina.alp26@gmail.com>
2020-12-03 18:36:20 +0200

    adds nothing

--- src/components/BgImage/BgImage.js
+++ src/components/BgImage/BgImage.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import style from './BgImage.module.css';
 const BgImage = () => {
-    return (
-        <div className={style.bgImage}></div>
-    );
+  return (
+    <div className={style.bgImage}></div>
+  );
 };
 
 export default BgImage;

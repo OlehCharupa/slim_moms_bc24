8032155ad2340d2b6c45cb546d6173e48403508f
TimChern111 <66830325+TimChern111@users.noreply.github.com>
2020-12-04 13:44:54 +0200

    jkl

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,20 @@ import PrivateRoute from "./PrivateRoute/PrivateRoute";
 import PublicRoute from "./PublicRoute/PublicRoute";
 import styles from "./App.module.css";
 import BgImage from "./BgImage/BgImage";
+import DiaryProductsListItem from "./DiaryProductsListItem/DiaryProductsListItem";
 
 function App() {
   return (
     <>
       <BgImage />
+      <DiaryProductsListItem />
       <div className={styles.container}>
         {routes.map((route) => {
           return route.private ? (
             <PrivateRoute key={route.label} {...route} />
           ) : (
-              <PublicRoute key={route.label} {...route} />
-            );
+            <PublicRoute key={route.label} {...route} />
+          );
         })}
       </div>
     </>
---
+++ src/components/DiaryProductsListItem/DiaryProductsListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./DiaryProductsListItem.module.css";
+
+const DiaryProductsListItem = () => {
+  return (
+    <div className={styles.container}>
+      <ul className={styles.list}>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+        <li className={styles.item}>Баклажан 100грамм 320ккал</li>
+      </ul>
+    </div>
+  );
+};
+
+export default DiaryProductsListItem;

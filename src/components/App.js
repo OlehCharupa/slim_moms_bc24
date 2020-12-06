0e306fdc44bc33d5ac23eb5dfba0bae5e2682016
kharchenkok <k.v.kharchenko@ukr.net>
2020-12-06 11:45:19 +0200

    main page added

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,28 @@
 import routes from "../routes/routes";
-import React from "react";
+import React, { Suspense } from "react";
 import PrivateRoute from "./PrivateRoute/PrivateRoute";
 import PublicRoute from "./PublicRoute/PublicRoute";
 import styles from "./App.module.css";
-import BgImage from "./BgImage/BgImage";
+import SpinerLoader from "./spinerLoader/SpinerLoader";
+import { Switch } from "react-router-dom";
+
 
 function App() {
   return (
     <>
-      <BgImage />
       <div className={styles.container}>
-        {routes.map((route) => {
-          return route.private ? (
-            <PrivateRoute key={route.label} {...route} />
-          ) : (
-              <PublicRoute key={route.label} {...route} />
-            );
-        })}
+        {/*для відображення сторінок Не видаляти! */}
+        <Suspense fallback={<SpinerLoader />}> 
+          <Switch>
+            {routes.map((route) => {
+              return route.private ? (
+                <PrivateRoute key={route.label} {...route} />
+              ) : (
+                <PublicRoute key={route.label} {...route} />
+              );
+            })}
+          </Switch>
+        </Suspense>
       </div>
     </>
   );

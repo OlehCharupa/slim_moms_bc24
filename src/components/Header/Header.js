07204114d1c7148eb74520012d65a218b65623dc
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2020-12-07 12:36:34 +0200

    Update Header.js
    заменяет шаблоны на компоненты

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,35 @@
 import React, { useState } from "react";
 import styles from "./Header.module.css";
-import { NavLink, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import viewport from "../../helpers";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import burger from "./images/burger.svg";
 import close from "./images/close.svg";
 import MenuModal from "../MenuModal/MenuModal";
 import MobileNavigation from "../MobileNavigation/MobileNavigation";
+import Logo from "../Logo/Logo";
+import Navigation from "../Navigation/Navigation";
+import UserInfo from "../UserInfo/UserInfo";
+
+import { isLogin } from "../../redux/selectors/selectors"; // тестовая логика, пока не заменят.
 
 const Header = () => {
-  //   const Authenticated = useSelector((state) => isAuthenticated(state));
-  const Authenticated = true;
+  const Authenticated = useSelector((state) => isLogin(state));
   const { pathname } = useLocation();
   const showDesktopNav =
-    pathname === "/login" || pathname === "/register" ? false : true;
+    pathname === "/login" || pathname === "/registration" ? false : true;
 
   const [modal, setModal] = useState(false);
   const [arrow, setArrow] = useState(true);
 
-  const modalHandler = () => setModal(!modal);
+  const body = document.querySelector("body");
+
+  const modalHandler = () => {
+    if (!modal) {
+      body.classList.add(styles.body);
+    } else body.classList.remove(styles.body);
+    setModal(!modal);
+  };
 
   const arrowToggle = () => setArrow(!arrow);
 
@@ -29,52 +40,38 @@ const Header = () => {
   return (
     <>
       <header className={styles.header}>
-        <div className={styles.logo}>Logo</div>
+        <Logo />
         {!viewport.isDesktop && Authenticated ? (
-          <button
-            type="button"
-            className={styles.btn}
-            aria-label="menu"
-            onClick={modalHandler}
-          >
-            {!modal && (
-              <img
-                src={burger}
-                alt="open menu"
-                aria-label="open menu"
-                width="32"
-                height="32"
-              />
-            )}
-            {modal && (
-              <img
-                src={close}
-                alt="close menu"
-                aria-label="close menu"
-                width="32"
-                height="32"
-              />
-            )}
-          </button>
+          <div className={styles.userBar}>
+            {Authenticated && viewport.isTablet && <UserInfo />}
+            <button
+              type="button"
+              className={styles.btn}
+              aria-label="menu"
+              onClick={modalHandler}
+            >
+              {!modal && (
+                <img
+                  src={burger}
+                  alt="open menu"
+                  aria-label="open menu"
+                  width="32"
+                  height="32"
+                />
+              )}
+              {modal && (
+                <img
+                  src={close}
+                  alt="close menu"
+                  aria-label="close menu"
+                  width="32"
+                  height="32"
+                />
+              )}
+            </button>
+          </div>
         ) : (
-          showDesktopNav && (
-            <div className={styles.nav}>
-              <NavLink
-                to="/login"
-                className={styles.link}
-                activeClassName={styles.activeLink}
-              >
-                Вход
-              </NavLink>
-              <NavLink
-                to="/register"
-                className={styles.link}
-                activeClassName={styles.activeLink}
-              >
-                Регистрация
-              </NavLink>
-            </div>
-          )
+          showDesktopNav && <Navigation />
         )}
       </header>
       {viewport.isMobile && (arrow || Authenticated) && (
@@ -83,28 +80,12 @@ const Header = () => {
           callback={arrowCallback}
           arrowToggle={arrowToggle}
         >
-          <span>Nick</span>
-          <span>Exit</span>
+          <UserInfo />
         </MobileNavigation>
       )}
       {modal && (
         <MenuModal>
-          <div className={styles.mobileNav}>
-            <NavLink
-              to="/login"
-              className={styles.link}
-              activeClassName={styles.activeLink}
-            >
-              Вход
-            </NavLink>
-            <NavLink
-              to="/register"
-              className={styles.link}
-              activeClassName={styles.activeLink}
-            >
-              Регистрация
-            </NavLink>
-          </div>
+          <Navigation />
         </MenuModal>
       )}
     </>

d22be8336e37131987e0fbee30f2aa536e3a60ca
Alina Pavliuk <alina.alp26@gmail.com>
2020-12-08 18:57:38 +0200

    fix conflicts

--- src/redux/slice/currentDateInfoSlice.js
+++ src/redux/slice/currentDateInfoSlice.js
@@ -1,11 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-<<<<<<< HEAD
   "eatenProducts": [],
   "currentDate": '',
   "daySummary": {},
-  "addProduct": []
 }
 
 const currentDateInfoSlice = createSlice({
@@ -25,34 +23,10 @@ const currentDateInfoSlice = createSlice({
       return { ...state, "eatenProducts": [...state.eatenProducts, payload.eatenProduct], "daySummary": payload.daySummary }
     },
   }
-=======
-    "eatenProducts": [],
-    "currentDate": '',
-    "daySummary": {}
-}
-
-const currentDateInfoSlice = createSlice({
-    name: "currentDateInfo",
-    initialState,
-    reducers: {
-        getCurrentDay(state, { payload }) {
-            return { ...state, "currentDate": payload }
-        },
-        setDateInfo(state, { payload }) {
-            return { ...state, "eatenProducts": payload.eatenProducts, "daySummary": payload.daySummary }
-        }
-
-
-    }
->>>>>>> dev
 })
 
 
 const { actions, reducer } = currentDateInfoSlice;
-<<<<<<< HEAD
 export const { getCurrentDay, setDateInfo, addProductRequest, addProductSuccess } = actions;
-=======
-export const { getCurrentDay, setDateInfo } = actions;
->>>>>>> dev
 
 export default reducer;

6ad7f2d335d53b50bb1a950107627cd7511827ec
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2020-12-08 12:47:37 +0200

    Update DailyCalorieIntake.js

--- src/components/DailyCalorieIntake/DailyCalorieIntake.js
+++ src/components/DailyCalorieIntake/DailyCalorieIntake.js
@@ -1,33 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import styles from "./DailyCalorieIntake.module.css";
 import Modal from "../modal/Modal";
+import {
+  notAllowedProducts,
+  dailyRate,
+  errorRequest,
+} from "../../redux/selectors/selectors";
 
-const DailyCalorieIntake = ({ ccal = 2800 }) => {
-  const products = ["Мучные продукты", "Молоко", "Красное мясо", "Копчености"];
+const DailyCalorieIntake = () => {
+  const history = useHistory();
+  const products = useSelector((state) => notAllowedProducts(state));
+  const ccal = useSelector((state) => dailyRate(state));
+  const error = useSelector((state) => errorRequest(state));
+
+  const [isModal, setIsModal] = useState(true);
+
+  const modalHandler = () => setIsModal(false);
+  const btnSubmit = () => {
+    modalHandler();
+    history.push("/registration");
+  };
+
   return (
-    <Modal>
-      <div className={styles.dailyCalorieIntake}>
-        <h1 className={styles.title}>
-          Ваша рекомендуемая суточная норма калорий составляет
-        </h1>
-        <p className={styles.info}>
-          <span className={styles.ccal}>{ccal}</span> ккал
-        </p>
-        <div className={styles.mustntEatDiv}>
-          <h2 className={styles.mustntEat}>
-            Продукты, которые вам <br /> не рекомендуется употреблять
-          </h2>
-          <ol className={styles.list}>
-            {products.map((product) => (
-              <li>{product}</li>
-            ))}
-          </ol>
+    <Modal openModal={isModal} toggleModal={modalHandler}>
+      {error ? (
+        <p className={styles.error}>Произошла ошибка, попробуйте позже!</p>
+      ) : (
+        <div className={styles.dailyCalorieIntake}>
+          <h1 className={styles.title}>
+            Ваша рекомендуемая суточная норма калорий составляет
+          </h1>
+          <p className={styles.info}>
+            <span className={styles.ccal}>{ccal}</span> ккал
+          </p>
+          <div className={styles.mustntEatDiv}>
+            <h2 className={styles.mustntEat}>
+              Продукты, которые вам <br /> не рекомендуется употреблять
+            </h2>
+            <ol className={styles.list}>
+              {products.map((product) => (
+                <li key={product}>{product}</li>
+              ))}
+            </ol>
+          </div>
+          <button type="button" className={styles.btn} onClick={btnSubmit}>
+            Начать худеть
+          </button>
         </div>
-        <button type="button" className={styles.btn}>
-          Начать худеть
-        </button>
-      </div>
+      )}
     </Modal>
   );
 };
